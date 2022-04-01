7c2f4deb647b80050cce977ca6718cd69160a02e
Martin Marmsoler <martin.marmsoler@gmail.com>
2022-04-01 13:59:15 +0200

    start xvfb

--- test/linux-runner.sh
+++ test/linux-runner.sh
@@ -3,4 +3,13 @@
 icewm&
 sleep 2
 
+if [[ -n ${XDISPLAY-} ]]; then
+  if [[ "$OSTYPE" != "darwin"* ]]; then
+    # 'coproc' so that SIGINT will still work (https://unix.stackexchange.com/a/478697/11592)
+    coproc Xvfb ${XDISPLAY} -screen 0 1024x768x16
+    VIRT_FB_PID=$!
+    sleep 4 # time to (probabilistically) ensure that Xvfb has started
+  fi
+fi
+
 exec ninja check
