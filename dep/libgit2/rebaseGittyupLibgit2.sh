232c1a96884d618a2b616798abdf904ee4dc17b6
Martin Marmsoler <martin.marmsoler@gmail.com>
2022-07-22 16:25:05 +0200

    add script to automatically rebase

---
+++ dep/libgit2/rebaseGittyupLibgit2.sh
@@ -0,0 +1,12 @@
+git checkout Gittyup
+git reset --hard upstream/main
+git merge --no-edit origin/context_line_accessor´
+git merge --no-edit origin/blame_abort
+git merge --no-edit origin/callback_connect_disconnect
+git merge --no-edit origin/disableRenameDetection
+git merge --no-edit origin/checkout_deletion_notification
+git merge --no-edit origin/blame_buffer
+git merge --no-edit origin/hash
+git merge --no-edit origin/disable_mmap
+git merge --no-edit origin/diff_checkout
+git merge --no-edit origin/fetch_annotated_tags
