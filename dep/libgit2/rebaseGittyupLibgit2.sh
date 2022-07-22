2e03f02db2a2614ff6db360d1d715af3e57bf137
Martin Marmsoler <martin.marmsoler@gmail.com>
2022-07-22 16:43:11 +0200

    add script to automatically rebasing to new libgit branch

--- dep/libgit2/rebaseGittyupLibgit2.sh
+++ dep/libgit2/rebaseGittyupLibgit2.sh
@@ -1,12 +1,25 @@
+declare -a branches=("origin/context_line_accessor´" "origin/blame_abort" "origin/callback_connect_disconnect" "origin/disableRenameDetection" "origin/checkout_deletion_notification" "origin/blame_buffer" "origin/hash" "origin/disable_mmap" "origin/diff_checkout" "origin/fetch_annotated_tags")
+
+for branch in ${branches[@]}; do
+	git checkout $branch
+	git rebase upstream/main
+done
+
 git checkout Gittyup
 git reset --hard upstream/main
-git merge --no-edit origin/context_line_accessor´
-git merge --no-edit origin/blame_abort
-git merge --no-edit origin/callback_connect_disconnect
-git merge --no-edit origin/disableRenameDetection
-git merge --no-edit origin/checkout_deletion_notification
-git merge --no-edit origin/blame_buffer
-git merge --no-edit origin/hash
-git merge --no-edit origin/disable_mmap
-git merge --no-edit origin/diff_checkout
-git merge --no-edit origin/fetch_annotated_tags
+
+for branch in ${branches[@]}; do
+	git merge --no-edit $branch
+done
+#git checkout Gittyup
+#git reset --hard upstream/main
+#git merge --no-edit "origin/context_line_accessor´"
+#git merge --no-edit "origin/blame_abort"
+#git merge --no-edit "origin/callback_connect_disconnect"
+#git merge --no-edit "origin/disableRenameDetection"
+#git merge --no-edit "origin/checkout_deletion_notification"
+#git merge --no-edit "origin/blame_buffer"
+#git merge --no-edit "origin/hash"
+#git merge --no-edit "origin/disable_mmap"
+#git merge --no-edit "origin/diff_checkout"
+#git merge --no-edit "origin/fetch_annotated_tags"
